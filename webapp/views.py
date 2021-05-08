47c9285a57f39c645d9451a848b9ad7cf3667e27
DASUCCESS <55310950+DASUCCESS@users.noreply.github.com>
2021-05-08 06:05:51 +0100

    Update views.py
    views.py changes completed

--- webapp/views.py
+++ webapp/views.py
@@ -34,36 +34,16 @@ def loginPage(request):
 
 @unauthenticated_user
 def registerPage(request):
-    
-    # form = CreateUserForm()
-	# if request.method == 'POST':
-	# 	form = CreateUserForm(request.POST)
-	# 	if form.is_valid():
-	# 		user = form.save()
-	# 		username = form.cleaned_data.get('username')
-
-	# 		group = Group.objects.get(name='customer')
-	# 		user.groups.add(group)
-
-	# 		messages.success(request, 'Account was created for ' + username)
-
-	# 		return redirect('login')
-
 	form = CreateUserForm()
 	if request.method == 'POST':
 		form = CreateUserForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			username = form.cleaned_data.get('username')
-   
-			# group = Group.objects.get(name='customer')
-			# user.groups.add(group)
-   
 			messages.success(request, 'Account was created for ' + username)
 
 			return redirect('login')
-			
-
+		
 	context = {'form':form}
 	
 	return render(request, 'signup.html', context)
@@ -145,84 +125,3 @@ def deleteOrder(request, pk):
 
 	context = {'order':order}
 	return render(request, 'delete.html', context)
-
-
-
-# @login_required(login_url='login')
-# def home(request):
-# 	orders = Order.objects.all()
-# 	customers = Customer.objects.all()
-
-# 	total_customers = customers.count()
-
-# 	total_orders = orders.count()
-# 	delivered = orders.filter(status='Delivered').count()
-# 	pending = orders.filter(status='Pending').count()
-
-# 	context = {'orders':orders, 'customers':customers,
-# 	'total_orders':total_orders,'delivered':delivered,
-# 	'pending':pending }
-
-# 	return render(request, 'accounts/dashboard.html', context)
-
-# @login_required(login_url='login')
-# def products(request):
-# 	products = Product.objects.all()
-
-# 	return render(request, 'accounts/products.html', {'products':products})
-
-# @login_required(login_url='login')
-# def customer(request, pk_test):
-# 	customer = Customer.objects.get(id=pk_test)
-
-# 	orders = customer.order_set.all()
-# 	order_count = orders.count()
-
-# 	myFilter = OrderFilter(request.GET, queryset=orders)
-# 	orders = myFilter.qs 
-
-# 	context = {'customer':customer, 'orders':orders, 'order_count':order_count,
-# 	'myFilter':myFilter}
-# 	return render(request, 'accounts/customer.html',context)
-
-# @login_required(login_url='login')
-# def createOrder(request, pk):
-# 	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10 )
-# 	customer = Customer.objects.get(id=pk)
-# 	formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
-# 	#form = OrderForm(initial={'customer':customer})
-# 	if request.method == 'POST':
-# 		#print('Printing POST:', request.POST)
-# 		form = OrderForm(request.POST)
-# 		formset = OrderFormSet(request.POST, instance=customer)
-# 		if formset.is_valid():
-# 			formset.save()
-# 			return redirect('/')
-
-# 	context = {'form':formset}
-# 	return render(request, 'accounts/order_form.html', context)
-
-# @login_required(login_url='login')
-# def updateOrder(request, pk):
-
-# 	order = Order.objects.get(id=pk)
-# 	form = OrderForm(instance=order)
-
-# 	if request.method == 'POST':
-# 		form = OrderForm(request.POST, instance=order)
-# 		if form.is_valid():
-# 			form.save()
-# 			return redirect('/')
-
-# 	context = {'form':form}
-# 	return render(request, 'accounts/order_form.html', context)
-
-# @login_required(login_url='login')
-# def deleteOrder(request, pk):
-# 	order = Order.objects.get(id=pk)
-# 	if request.method == "POST":
-# 		order.delete()
-# 		return redirect('/')
-
-# 	context = {'item':order}
-# 	return render(request, 'accounts/delete.html', context)
